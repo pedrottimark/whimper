1dffdfcd31a1502b73c0dcdb4505e0cb252dc6b4
Mark Pedrotti <pedrottimark@gmail.com>
2017-03-15 14:41:41 -0400

    delete input element from snapshot so test fails

--- src/components/__tests__/Table-V.test.js
+++ src/components/__tests__/Table-V.test.js
@@ -16,84 +16,12 @@ import {
   recordB,
   recordC,
   recordD,
-  recordE,
-  recordF,
-  recordG,
 } from '../../testing/records-data';
 
 import Table from '../Table';
 const TableRow = () => {}; // mock
 
 const tbodyShallow = ($it) => mountToShallowObject($it.find('tbody'));
-
-describe('Table filtering', () => {
-  const changeFilter = ($input, value) => {
-    $input.get(0).value = value;
-    $input.simulate('change');
-  };
-  const countRecords = ($it) => Number($it.find('thead tr').at(1).find('th').at(0).text());
-
-  const records = [recordA, recordB, recordC, recordD, recordE, recordF, recordG];
-  const store = createStore(reducer);
-  store.dispatch(receiveData(fields, records));
-  const $it = mount(
-    <Provider store={store}>
-      <Table />
-    </Provider>
-  );
-  const $input = $it.find('thead input');
-
-  it('matches all rows', () => {
-    changeFilter($input, '1');
-    expect(countRecords($it)).toEqual(records.length);
-  });
-
-  it('matches fewer rows', () => {
-    changeFilter($input, '10');
-    expect(countRecords($it)).toEqual(2);
-    expect(tbodyShallow($it)).toMatchObject(relevantTestObject(
-      <tbody>
-        <TableRow record={recordD} />
-        <TableRow record={recordG} />
-      </tbody>
-    ));
-  });
-
-  it('matches same rows', () => {
-    changeFilter($input, '10 ');
-    expect(countRecords($it)).toEqual(2);
-    expect(tbodyShallow($it)).toMatchObject(relevantTestObject(
-      <tbody>
-        <TableRow record={recordD} />
-        <TableRow record={recordG} />
-      </tbody>
-    ));
-  });
-
-  it('matches even fewer rows', () => {
-    changeFilter($input, '10 y');
-    expect(countRecords($it)).toEqual(1);
-    expect(tbodyShallow($it)).toMatchObject(relevantTestObject(
-      <tbody>
-        <TableRow record={recordD} />
-      </tbody>
-    ));
-  });
-
-  it('matches no rows', () => {
-    changeFilter($input, '10 yr');
-    expect(countRecords($it)).toEqual(0);
-    expect(tbodyShallow($it)).toMatchObject(relevantTestObject(
-      <tbody>
-      </tbody>
-    ));
-  });
-
-  it('matches all rows again', () => {
-    changeFilter($input, '');
-    expect(countRecords($it)).toEqual(records.length);
-  });
-});
 
 describe('Table sorting', () => {
   const clickHeading = ($it, i) => {
