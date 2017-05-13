cd5958f12469496d4c53d6b53eb69cf266bcd3ec
Mark Pedrotti <pedrottimark@gmail.com>
2017-05-13 10:31:18 -0400

    Simplify relevantTestObject

--- src/testing/react-test-renderer/relevantTestObject.js
+++ src/testing/react-test-renderer/relevantTestObject.js
@@ -1,12 +1,7 @@
-'use strict';
-
-import omitBy from 'lodash.omitby';
-import isNil from 'lodash.isnil';
-
 const reactSymbol = Symbol.for('react.element');
 const testSymbol = Symbol.for('react.test.json');
 
-const irrelevant = '\u0007'; // BELL
+const irrelevant = '\u0007'; // BELL is tentative sentinel string
 
 function typeOfElement(type) {
   if (typeof type === 'string') {
@@ -18,18 +13,21 @@ function typeOfElement(type) {
   return 'Unknown';
 }
 
-function childrenInTestObject(arg, array) {
+// Given the value of or any item in children prop of React element,
+// return a flattened array of test objects and text nodes.
+function childrenOfTestObject(arg, array = []) {
   if (Array.isArray(arg)) {
-    // Iteration adjacent to other rendered output: { array.map(item => node) }
-    return arg.reduce((reduced, child) => childrenInTestObject(child, reduced), array);
-  }
-
-  if(!isNil(arg) && typeof arg !== 'boolean') {
+    // Either multiple children or
+    // iteration adjacent to other rendered output: { array.map(item => node) }
+    arg.forEach(child => { childrenOfTestObject(child, array); });
+  } else if (arg !== null && arg !== undefined && typeof arg !== 'boolean') {
+    // Omit item if it is any of the following: null, undefined, boolean.
     array.push(arg && arg.$$typeof === reactSymbol
-      ? relevantTestObject(arg) // recurse for object
-      : arg // non-element node
+      ? relevantTestObject(arg) // recurse if child is React element
+      : arg // non-element node, especially text
     );
   }
+
   return array;
 }
 
@@ -37,29 +35,28 @@ function childrenInTestObject(arg, array) {
 // `$$typeof` is enumerable
 // `props` is undefined if there are no non-children properties
 // `children` is undefined if they are irrelevant
-function relevantTestObject(arg) {
-  const {type, props} = arg;
+function relevantTestObject(element) {
   const object = {
     $$typeof: testSymbol,
-    type: typeOfElement(type),
+    type: typeOfElement(element.type),
   };
 
+  const {children, ...props} = element.props;
+
   // Omit `props` if there are no properties, not counting `children`.
-  const keys = Object.keys(props);
-  const children = props.children;
-  if (keys.length !== (children ? 1 : 0)) {
-    object.props = omitBy(props, (val, key) => key === 'children');
+  if (Object.keys(props).length !== 0) {
+    object.props = props;
   }
 
   // Omit `children` is it consists of the `irrelevant` sentinel value.
   if (children !== irrelevant) {
-    if (typeof children === 'undefined') {
+    if (children === undefined) {
       object.children = null;
     } else {
-      object.children = childrenInTestObject(children, []);
-      if (object.children.length === 0) {
-        object.children = null;
-      }
+      const childrenFlattened = childrenOfTestObject(children);
+      object.children = childrenFlattened.length !== 0
+        ? childrenFlattened
+        : null;
     }
   }
 
